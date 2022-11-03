a1af45be2e7630440d92a715a33d7e925d586769
Mario Loera <marioll@kth.se>
2022-11-03 15:51:42 +0100

    fix backend bucket

--- test/terraform.tf
+++ test/terraform.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "gcs" {
-    bucket = "trustly_infra_data_services_test"
-    prefix = "terraform/infra-data-services"
+    bucket = "turing-app-367309-terraform-infra"
+    prefix = "tft/state"
   }
 }
