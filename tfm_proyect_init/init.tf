c5a7a9d7af27f074ae325fc31d5a065c85c627d2
Mario Loera <marioll@kth.se>
2022-11-07 14:29:26 +0100

    Init pa mll 01 (#1)
    * add status after applying init
    
    * excludes lock
    
    * use string variables similar to python f strings instead of format
    
    * google version = "~> 4.42.1"
    
    * use with google_project_iam_member
    
    * clean terraform GitHub workflow

--- tfm_proyect_init/init.tf
+++ tfm_proyect_init/init.tf
@@ -1,7 +1,7 @@
 # to initialize terraform in GCP create a terraform_admin service accout for terrafrom state storage bucket
 provider "google" {
   project = "turing-app-367309"
-  region  = "europe-north1"
+  region  = "europe-west1"
 }
 
 terraform {
@@ -10,27 +10,27 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "~> 4.10.0"
+      version = "~> 4.42.1"
     }
 
   }
 
 }
 
+data "google_project" "current_project" {
+}
 
-resource "google_service_account" "terraform_admin" {
-  account_id = "terraform-admin"
+resource "google_service_account" "svc_terraform_admin" {
+  account_id = "svc-terraform-admin"
 }
 
 
-resource "google_service_account_iam_binding" "svc_terraform_admin_owner" {
-  service_account_id = google_service_account.terraform_admin.name
-  role               = "roles/owner"
-
-  members = [
-    format("serviceAccount:%s", google_service_account.terraform_admin.email),
-  ]
+resource "google_project_iam_member" "svc_terraform_admin_owner" {
+  project = data.google_project.current_project.id
+  role    = "roles/owner"
+  member  = "serviceAccount:${google_service_account.svc_terraform_admin.email}"
 }
+
 
 
 module "bucket_terraform_infra" {
@@ -38,6 +38,7 @@ module "bucket_terraform_infra" {
 
   name = "terraform-infra"
   object_admin = [
-    format("serviceAccount:%s", google_service_account.terraform_admin.email),
+    # format("serviceAccount:%s", google_service_account.svc_terraform_admin.email),
+    "serviceAccount:${google_service_account.svc_terraform_admin.email}",
   ]
 }
