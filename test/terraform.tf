7c6a87fece0915ef719df0b10f44b098baa238fe
Mario Loera <marioll@kth.se>
2022-11-08 20:26:29 +0100

    locally run and workded

--- test/terraform.tf
+++ test/terraform.tf
@@ -1,7 +1,7 @@
 terraform {
-  backend "gcs" {
-    # TODO MANUALLY change the bucket
-    bucket = "pa-cons-swe-de-2022-mll03-terraform-infra"
-    prefix = "tft/state"
-  }
+  # backend "gcs" {
+  #   # TODO MANUALLY change the bucket
+  #   bucket = "pa-cons-swe-de-2022-mll03-terraform-infra"
+  #   prefix = "tft/state"
+  # }
 }
