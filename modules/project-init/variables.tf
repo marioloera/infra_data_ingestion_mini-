7b8311ed05fff6833b3474ec516c65cc59c8e555
Mario Loera <marioll@kth.se>
2022-11-08 20:11:09 +0100

    create module project-init for depend on

---
+++ modules/project-init/variables.tf
@@ -0,0 +1,4 @@
+variable "gcp_project" {}
+variable "main_user" {}
+variable "terraform_state_bucket_name" {}
+variable "svc_terraform_admin_name" {}
--- tfm_proyect_init/init.tf
+++ tfm_proyect_init/init.tf
@@ -26,60 +26,15 @@ resource "google_project" "my_project" {
   billing_account = local.billing_account
 }
 
-# data "google_project" "current_project" {
-# }
+module "init_project_module" {
+  source = "../modules/project-init"
 
-# *********************** SERVICE ACCOUNT ***********************
-resource "google_service_account" "svc_terraform_admin" {
-  account_id = local.svc_terraform_admin_name
-}
-
-
-# *********************** PROJECT POLICIES ***********************
-resource "google_project_iam_member" "svc_terraform_admin_owner" {
-  project = local.gcp_project
-  role    = "roles/owner"
-  member  = "serviceAccount:${google_service_account.svc_terraform_admin.email}"
-}
-
-resource "google_project_iam_binding" "storage_admin" {
-  #project = data.google_project.current_project.id
-  project = local.gcp_project
-  role    = "roles/storage.admin"
-  members = [
-    "user:${local.main_user}",
-    "serviceAccount:${google_service_account.svc_terraform_admin.email}",
-  ]
-}
-
-# *********************** STORAGE ***********************
-resource "google_storage_bucket" "bucket_terraform_infra" {
-  name                        = "${local.gcp_project}-${local.terraform_state_bucket_name}"
-  project                     = local.gcp_project
-  location                    = "EU"
-  force_destroy               = true
-  uniform_bucket_level_access = true
-  requester_pays              = true
-  versioning {
-    enabled = true
-  }
+  gcp_project                 = local.gcp_project
+  main_user                   = local.main_user
+  terraform_state_bucket_name = local.terraform_state_bucket_name
+  svc_terraform_admin_name    = local.svc_terraform_admin_name
 
   depends_on = [
-    google_project_iam_binding.storage_admin,
+    google_project.my_project,
   ]
-}
-
-data "google_iam_policy" "bucket_terraform_infra_policy" {
-  binding {
-    role = "roles/storage.objectAdmin"
-    members = [
-      "user:${local.main_user}",
-      "serviceAccount:${google_service_account.svc_terraform_admin.email}",
-    ]
-  }
-}
-
-resource "google_storage_bucket_iam_policy" "bucket_terraform_infra_policy_mapping" {
-  bucket      = google_storage_bucket.bucket_terraform_infra.name
-  policy_data = data.google_iam_policy.bucket_terraform_infra_policy.policy_data
 }
