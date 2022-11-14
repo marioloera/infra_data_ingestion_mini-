31de351fb070edbce6d291738a265950ab04835b
Mario Loera <marioll@kth.se>
2022-11-14 14:37:01 +0100

    add serviceAccountUser to vc-application-deployment

--- test/service-account-policies.tf
+++ test/service-account-policies.tf
@@ -9,3 +9,9 @@ resource "google_service_account_iam_binding" "svc_dataflow_worker_user" {
     format("serviceAccount:%s", google_service_account.svc_application_deployment.email),
   ]
 }
+
+resource "google_project_iam_member" "svc_compute_user" {
+  project = data.google_project.current_project.id
+  role    = "roles/iam.serviceAccountUser"
+  member  = "serviceAccount:${google_service_account.svc_application_deployment.email}"
+}
